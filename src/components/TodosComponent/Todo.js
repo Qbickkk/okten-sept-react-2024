eb22d315e8f1300285d2dae3e5479ef37b5bcc39
shulika <illia.shulika@gmail.com>
2024-02-13 20:21:14 -0800

    hw4

--- src/components/TodosComponent/Todo.js
+++ src/components/TodosComponent/Todo.js
@@ -9,7 +9,7 @@ const Todo = ({todo}) => {
             <div>userId:{userId}</div>
             <div>id:{id}</div>
             <div>title:{title}</div>
-            <div>completed:{completed}</div>
+            <div>completed:{completed.toString()}</div>
             <br/>
         </div>
     );
